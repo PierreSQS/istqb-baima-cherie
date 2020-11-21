73c8c3f49de866c51ce85b63b5751130bbce1656
PierreSQS <nomo_messina@web.de>
2020-11-21 23:25:36 +0100

    Commit 15-OK:added a Repository in the Controller and added an age field
    on the LoginForm

--- example-istqb-odile/src/main/java/com/example/demo/controllers/LoginController.java
+++ example-istqb-odile/src/main/java/com/example/demo/controllers/LoginController.java
@@ -1,7 +1,10 @@
 package com.example.demo.controllers;
 
+import java.time.LocalDateTime;
+
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,12 +13,16 @@ import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.example.demo.entities.LoginData;
+import com.example.demo.persistence.LoginRepository;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
 public class LoginController implements WebMvcConfigurer{
+	
+	@Autowired
+	private LoginRepository loginRepo;
 	
 	// for any redirection a Handler should be implemented
 	// this is a way to do it without a Handler-Method
@@ -34,7 +41,12 @@ public class LoginController implements WebMvcConfigurer{
 		if (error.hasErrors()) {
 			return "loginForm";
 		}
+		
+		LocalDateTime today = LocalDateTime.now();
+		pLogin.setLoggedAt(today);
+		loginRepo.save(pLogin);
 		log.info("redirecting to /loginSuccess");
+		
 		return "redirect:/loginOk";
 	}
 
