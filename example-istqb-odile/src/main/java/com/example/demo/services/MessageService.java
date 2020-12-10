baa3e4db36617382f584a0e1099aa71683aaba7e
PierreSQS <nomo_messina@web.de>
2020-12-10 21:17:08 +0100

    Commit 28-NOK: Extended the Message Service to full-fill the
    requirements. The Integration-Test is now broken.

--- example-istqb-odile/src/main/java/com/example/demo/services/MessageService.java
+++ example-istqb-odile/src/main/java/com/example/demo/services/MessageService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.domain.Message;
 import com.example.demo.entities.LoginData;
+import com.example.demo.persistence.LoginRepository;
 import com.example.demo.utils.validation.LoginValidation;
 
 @Service
@@ -11,13 +12,19 @@ public class MessageService {
 
 	private LoginValidation validation;
 
-//	private LoginRepository loginRepo;
-//
-//	public MessageService(LoginValidation validation, LoginRepository loginRepo) {
-//		super();
-//		this.validation = validation;
-//		this.loginRepo = loginRepo;
-//	}
+	private LoginRepository loginRepo;
+	
+	public MessageService() {
+		
+	}
+	
+	// Autowired is set per Default since Spring 5.0
+	// and is set for both Constructors 
+	public MessageService(LoginValidation validation, LoginRepository loginRepo) {
+		super();
+		this.validation = validation;
+		this.loginRepo = loginRepo;
+	}
 
 	public MessageService(LoginValidation validation) {
 		super();
@@ -26,19 +33,27 @@ public class MessageService {
 
 	public Message getMessage(LoginData pLoginData) {
 		String ageStatus = validation.ageValidation(pLoginData.getAge());
+		Message returnedMsg = null;
 		switch (ageStatus) {
 		case "MINEUR" :
-			return new Message(1, "XY");			
+			returnedMsg = new Message(1, "XY");	
+			break;
 
 		case "ADULTE" :
-			return new Message(2, "AB");			
+			returnedMsg = new Message(2, "AB");
+			break;
 
 		case "SENIOR" :
-			return new Message(3, "CD");			
+			returnedMsg = new Message(3, "CD");
+			break;
 
 		default:
-			return new Message(4, "FG");
+			returnedMsg = new Message(4, "FG");
+			break;
 		}
+		
+		loginRepo.save(pLoginData);
+		return returnedMsg;
 	
 	}
 }
