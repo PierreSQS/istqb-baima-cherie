259ae6671a2b0e568a0cca037ffa8b10b34aede6
PierreSQS <nomo_messina@web.de>
2020-12-11 13:48:26 +0100

    Commit 34-OK: Hurra!!! Now the test ist passing!!

--- example-istqb-odile/src/test/java/com/example/demo/services/MessageServiceUnitTest.java
+++ example-istqb-odile/src/test/java/com/example/demo/services/MessageServiceUnitTest.java
@@ -1,15 +1,16 @@
 package com.example.demo.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.example.demo.configuration.TestConfig;
+import com.example.demo.domain.Message;
 import com.example.demo.entities.LoginData;
 import com.example.demo.persistence.LoginRepository;
 
@@ -22,17 +23,20 @@ class MessageServiceUnitTest {
 	@Autowired
 	private MessageService msgServTO;
 	
-	@MockBean
+	@Autowired
 	private LoginRepository loginRepoMock;
 
 	@Test
-	void test() {
+	void testShouldReturnMessageXY() {
 		// Arrange
 		fLoginData = new LoginData(10);
 		when(loginRepoMock.save(fLoginData)).thenReturn(fLoginData);
 		
-		// act
-		msgServTO.getMessage(fLoginData);
+		// Act
+		Message msg = msgServTO.getMessage(fLoginData);
+		
+		// Assert
+		assertEquals("XY", msg.getMessage());
 	}
 
 }
