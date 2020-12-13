2b3b2645dba34e0d0b9701c81399a24db299241a
PierreSQS <nomo_messina@web.de>
2020-12-13 19:15:07 +0100

    Commit 41-OK: First Attempt to introduce internationalized messages

---
+++ example-istqb-odile/src/main/java/com/example/demo/config/MessageConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+
+@Configuration
+public class MessageConfig {
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+	    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+	    messageSource.setBasename("Message_FR");
+	    return messageSource;
+	}
+}
