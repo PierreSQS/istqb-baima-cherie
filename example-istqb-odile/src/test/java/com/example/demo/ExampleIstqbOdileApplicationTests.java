8a246057077a65d3b282ab05f2ec3314455c6d96
PierreSQS <nomo_messina@web.de>
2021-05-27 04:04:15 +0200

    Commit60-Ok: Implemented Integration Test

--- example-istqb-odile/src/test/java/com/example/demo/ExampleIstqbOdileApplicationTests.java
+++ example-istqb-odile/src/test/java/com/example/demo/ExampleIstqbOdileApplicationTests.java
@@ -2,12 +2,19 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class ExampleIstqbOdileApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext appCtx) {
+		int beanDefinitionCount = appCtx.getBeanDefinitionCount();
+		assertNotNull(appCtx);
+		assertThat(beanDefinitionCount).isGreaterThan(10);
 	}
 
 }
